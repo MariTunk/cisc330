e476430076fb92b7ac91946aa42a90d9000dfd60
Mariyam Tunkara <Mariyamtunkara456@gmail.com>
2025-02-24 17:14:53 -0500

    Update index.php

--- inClass12/php-app/index.php
+++ inClass12/php-app/index.php
@@ -7,3 +7,48 @@ echo '
     '
 
 ?>
+
+
+
+$username = 'Mariyam';
+$greeting = 'Hello' . ' ' . $username . '.';
+
+$coffee = [
+  'item' => 'drink',
+  'qty' => 2,
+  'price' => 10,
+  'discount' => 3
+];
+$usualPrice = $coffee['qty'] * $coffee['price'];
+$offerPrice = $coffee['qty'] * $coffee['discount'];
+$saving = $usualPrice - $offerPrice;
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="styles.css">
+    <title>The Store</title>
+  </head>
+  <body>
+    <div class="container">
+      <div>
+        <h1>The Store</h1>
+
+        <h2>Multi-buy Offer</h2>
+
+        <!--shorthand for echo-->
+        <p><?= $greeting ?></p>
+
+        <p class="sticker">Save <?= $saving ?></p>
+
+        <p>Buy <?= $coffee['qty'] ?> packs of <?= $coffee['item'] ?>
+          for <?= $offerPrice ?><br>(usual price $<?= $usualPrice ?>)</p>
+      </div>
+      <div>
+        <img src="shopping.webp" class="drinks">
+      </div>
+    </div>
+
+  </body>
+</html>
