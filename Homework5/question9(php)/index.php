65ce65360f602027bd0a2d41cb338927d05bb74d
Mariyam Tunkara <Mariyamtunkara456@gmail.com>
2025-02-27 22:28:31 -0500

    Update index.php

--- Homework5/question9(php)/index.php
+++ Homework5/question9(php)/index.php
@@ -2,29 +2,31 @@
 //we need this to accept requests from any domain
 header("Access-Control-Allow-Origin: *");
 
-$uri = strtok($_SERVER["REQUEST_URI"], '?');
+$uri = strtok($_SERVER["REQUEST_Henna"], '?');
 
 //get uri pieces
-$uriArray = explode("/", $uri);
+$hennaArray = explode("/", $henna);
 //var_dump($uriArray);
 //0
 //1 forms
 
-if ($uriArray[1] === 'cats' && $_SERVER['REQUEST_METHOD'] === 'GET') {
-    $cats = [
+if ($hennaArray[1] === 'jagua' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+    $henna = [
         [
-            'name' => 'pinecone'
+            'itemName' => 'jagua henna'
+            'price' => '$45'
         ],
         [
-            'name' => 'Mr'
+           'itemName' => 'Red henna'
+            'price' => '$25' 
         ]
     ];
 
-    echo json_encode($cats);
+    echo json_encode($henna);
     exit();
 }
 
-if ($uriArray[1] === 'form' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($hennaArray[1] === 'form' && $_SERVER['REQUEST_METHOD'] === 'POST') {
     echo json_encode([
         'message' => 'Success'
     ]);
