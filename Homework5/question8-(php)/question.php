2c9eaf6fef176678b7a8ed4f5987e450c2617730
Mariyam Tunkara <Mariyamtunkara456@gmail.com>
2025-02-27 21:35:37 -0500

    Create question.php

---
+++ Homework5/question8-(php)/question.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Events</title>
+    </head>
+    <body>
+      
+        <div id="initial-text" onclick="initialReact()">
+            initial text
+        </div>
+
+        <script>
+
+        function initialReact() {
+            let div = document.getElementById('initial-text');
+            div.innerText = 'added text!';
+            div.style.backgroundColor = 'yellow';
+        }
+
+        </script>
+
+    </body>
+</html>
