b1e96799bf7b4a009fbd5e9b08c62288cb7373ec
Mariyam Tunkara <Mariyamtunkara456@gmail.com>
2025-03-03 17:06:27 -0500

    Update index.php

--- inClass14/Php Project/index.php
+++ inClass14/Php Project/index.php
@@ -1,1 +1,19 @@
+<?php
 
+//require 'classes/Cat.php';
+require 'classes/Dog.php';
+//require 'inheritance/Car.php';
+//require 'inheritance/Mustang.php';
+
+//use chapterFour\classes\Cat;
+use phpProject\business;
+//use chapterFour\inheritance\Mustang;
+
+
+$dog1 = new Dog('Daisy', 12);
+$dog2 = new Dog('Sophie', 5);
+
+echo $dog1->name;
+echo $dog2->name;
+
+>
