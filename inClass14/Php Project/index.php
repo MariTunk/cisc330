3ae3c509b85e627d003b5251cc72c05e28a47bfb
Mariyam Tunkara <Mariyamtunkara456@gmail.com>
2025-03-03 22:27:20 -0500

    Update index.php

--- inClass14/Php Project/index.php
+++ inClass14/Php Project/index.php
@@ -1,12 +1,14 @@
 <?php
 
-require 'Users\mariiii\homework6/business.php';
+//require 'Users\mariiii\homework6/business.php';
+require Php_Project\business;
 
 //use Users\mariiii\homework6\business;
 
-
-use Users\mariiii\homework6\Item;
-use Users\mariiii\homework6\Henna;
+use Php_Project\business\Item;
+use Php_Project\business\Henna;
+//use Users\mariiii\homework6\Item;
+//use Users\mariiii\homework6\Henna;
 
 $item1 = new Item('Jagua', 32);
 $item2 = new Item('Red', 10);
