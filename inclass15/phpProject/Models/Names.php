90ab8dbbd4a8ae1b4b944b90b23775bf1f51bdfc
Mariyam Tunkara <Mariyamtunkara456@gmail.com>
2025-03-06 18:59:49 -0500

    Update Names.php

--- inclass15/phpProject/Models/Names.php
+++ inclass15/phpProject/Models/Names.php
@@ -11,7 +11,21 @@ class user {
           [
             'Guest' => '2',
             'name' => 'Nathan'
-          ];
+          ],
+           [
+            'Guest' => '3',
+            'name' => 'Mariyam'
+          ];    
+        
+
+        if (!empty($params['name'])) {
+            return array_filter($allUsers, function ($user) use ($params) {
+                if (str_contains(strtolower($user['name']), $params['name'])) {
+                    return $user;
+                };
+                return null;
+            });
+        }
   }
 
 return $allUsers;
