2885bd750cd808884fe18a63f9d55b4709d104e8
Mariyam Tunkara <Mariyamtunkara456@gmail.com>
2025-03-06 16:47:37 -0500

    Update .php

--- inclass15/phpProject/Models/.php
+++ inclass15/phpProject/Models/.php
@@ -1,1 +1,18 @@
+<?php
 
+namespace phpProject\Models;
+
+class user {
+    public function getAllUsersByName($params) {
+           $associativeArray = [
+           'key1' => 'value1',
+           'key2' => 2,
+          ],
+          [
+                'id' => '2',
+                'name' => 'nathan'
+          ],
+  }
+
+return $allUsers;
+}
