63b3fef0c069607d27e4af214c6a1384fa53aa74
Mariyam Tunkara <Mariyamtunkara456@gmail.com>
2025-03-05 19:42:43 -0500

    Update index.php

--- Homework6/phpProject/index.php
+++ Homework6/phpProject/index.php
@@ -1,1 +1,22 @@
 
+<?php
+
+$myArray= [
+    'guests'=> '1',
+    'name'=> 'Mariyam',
+    'age'=> '34',
+    'Country'=> 'Maldives'
+    ];
+
+
+    function echoArrayAsString(array $array= ['guests' =>'1','name'=> 'Mariyam', 'age'=> '34','Country'=> 'Maldives']): void
+    {
+        foreach($array as $key => $value){
+            echo "{$key}:{$value}}";
+            echo '<br>';
+        }
+    }
+    echoArrayAsString(array: $myArray);
+    //echoArrayAsString();
+
+?>
