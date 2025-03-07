303fce2a3cd6512b2d81ee7206326f60ebbcd3d0
Mariyam Tunkara <Mariyamtunkara456@gmail.com>
2025-03-06 19:04:19 -0500

    Update router.php

--- inclass15/phpProject/router.php
+++ inclass15/phpProject/router.php
@@ -21,22 +21,22 @@ class router {
 
     protected function userRoutes($uriArray) {
         if ($uriArray[1] === 'api' && $uriArray[2] === 'users' && $_SERVER['REQUEST_METHOD'] === 'GET') {
-            $userController = new UserController();
-            $userController->getUsers();
+            $Control = new Control();
+            $Control->getUsers();
         }
 
         if ($uriArray[1] === 'api' && $uriArray[2] === 'users' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-            $userController = new UserController();
-            $userController->saveUser();
+            $Control = new Control();
+            $Control->saveUser();
         }
 
         if ($uriArray[1] === 'users-add' && $_SERVER['REQUEST_METHOD'] === 'GET') {
-            $userController = new UserController();
-            $userController->viewAddUsers();
+            $Control = new Control();
+            $Control->viewAddUsers();
         }
 
-        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-            require './views/users.html';
+        //if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+          //  require './views/users.html';
             exit();
         }
 
