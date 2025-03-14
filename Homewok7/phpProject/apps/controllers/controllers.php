06cbb640447489c6b3a03b18093e101d811879e4
Mariyam Tunkara <Mariyamtunkara456@gmail.com>
2025-03-13 23:26:50 -0400

    Update controllers.php

--- Homewok7/phpProject/apps/controllers/controllers.php
+++ Homewok7/phpProject/apps/controllers/controllers.php
@@ -1,43 +1,84 @@
 <?php
 
-namespace app\controllers;
+namespace controller;
 
-use Exception;
-use Error;
+//require "../controller/noteController.php";
 
-function myErrorHandler($errno, $errstr, $errfile, $errline) {
-//    echo "<b>Custom error:</b> [$errno] $errstr<br>";
-//    echo " Error on line $errline in $errfile<br>";
-    echo 'my error handler called';
-    exit();
-}
-class ErrorController {
+use controllers\controller;
 
-    public function warningFunction() {
-        preg_match('There will be a warning about missing delimiter here!', 'test');
+class Note
+{
+    public function getUsers() {
+        $params = [
+            //pincone
+            'title' => $_GET['title'] ?? null,
+            'description' => $_GET['description'] ?? null,
+        ];
+        $userModel = new Note();
+        $users = $userModel->getAllUsersByName($params);
+        echo json_encode($users);
+        exit();
     }
 
-    public function errorFunction() {
-        $price = 7;
-        $quantity = 'five';
-        $price * $quantity;
-    }
-    public function viewErrors() {
-//        $this->badFunction();
-//        $this->warningFunction();
-        try {
-//        echo 'Find an error";
-//            $this->badFunction();
-//            if (true) {
-//                throw new Exception('Custom error message!');
-//            }
-        } catch (Error $error) {
-            echo $error;
+    public function saveUser() {
+        //get post data from our form post
+        //{name: 'pinecone', age: '14'}
+        $title = $_POST['title'] ?? null;
+        $description = $_POST['description'] ?? null;
+        $errors = [];
+
+        //validate and sanitize name
+        if ($title) {
+            
+            $title = htmlspecialchars($title);
+
+            //echo ($title);
+            //echo '<br>';
+            //echo htmlspecialchars(htmlspecialchars($title));
+
+            //validate text length
+            if (strlen($title) < 4) {
+                $errors['title'] = 'Title must be at least 4 characters long';
+            }
+        } else {
+            $errors['title'] = 'Title is required';
         }
 
-        //set a custom function for errors
-        set_error_handler("app\controllers\myErrorHandler");
-        trigger_error('');
+        if ($description) {
+            $description = htmlspecialchars($description);
 
+            //echo ($description);
+            //echo '<br>';
+            //echo htmlspecialchars(htmlspecialchars($description));
+
+            //validate text length
+            if (strlen($description) < 11) {
+                $errors['description'] = 'Description must be at least 11 characters long';
+            }
+        } else {
+            $errors['description'] = 'Description is required';
+        }
+
+       
+        //if we have errors
+        if (count($errors)) {
+            http_response_code(400);
+            echo json_encode($errors);
+            exit();
+        }
+
+        $returnData = [
+            'title' => $title,
+            'description' => $description,
+            'message' => 'Request is successful!'
+        ];
+        echo json_encode($returnData);
+        exit();
     }
+
+    public function viewAddUsers() {
+        require './view/messageVal.html';
+        exit();
+    }
+
 }
